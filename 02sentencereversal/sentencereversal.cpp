627a8a5b3d83d8b913d663bc5445167172897973
liushui9404 <liushui9404@163.com>
2017-10-29 17:15:07 +0800

    second

--- 02sentencereversal/sentencereversal.cpp
+++ 02sentencereversal/sentencereversal.cpp
@@ -1,23 +1,27 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include<iostream>
+#include<algorithm>
+#include<string>
+
 using namespace std;
 
 int main()
 {
-    string words;
-    while(getline(cin, words))
+    string sentence;
+    while(getline(cin,sentence))
     {
-        reverse(words.begin(), words.end());
-        unsigned i = 0, j = i;
-        while(i < words.size())
+        reverse(sentence.begin(),sentence.end());
+        unsigned i = 0,j = 1;
+        while(i<sentence.size())
         {
-            while(i < words.size() && words[i] != ' ')
-                ++i;
-            reverse(words.begin() + j, words.begin() + i);
-            j = ++i;
-        }
-        cout << words << endl;
-    }
-    return 0;
+            while(i<sentence.size() && sentence[i] != ' ')
+            {
+                ++i;
+            }
+            reverse(sentence.begin()+j,sentence.begin()+i);
+            j = ++i;
+        }
+        cout<<sentence<<endl;
+
+    }
+    return 0;
 }
